9c134d7b43656ac065a8cf0c2acc4336c326c08f
Mike <143658803+MichaelVera88@users.noreply.github.com>
2025-08-25 15:20:04 -0400

    Add previous Java assignments

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+//Apologies, I had cleaned out my assignments from my previous Java courses.
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+}
