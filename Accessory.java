5c08c7c20f08908f9acb2579e5e796912828658a
Michael Vera <michaelgvera88@gmail.com>
2025-10-27 15:50:45 -0400

    Added Comments to Stat Class

--- Accessory.java
+++ Accessory.java
@@ -1,19 +1,30 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+* Stats Class:
+*   - Takes 2 arrays:
+*       - stat names
+*       - corresponding stat values
+*   - Combines & returns both arrays into a hashmap with the key being the name
+*/
 class Stats
 {
+    // Instance Variables
     private String[] statNames;
     private int[] statValues;
 
+    // Initialization
     public Stats(String[] statNames, int[] statValues)
     {
         this.statNames = statNames;
         this.statValues = statValues;
     }
 
+    // Combine into Hashmap
     public Map<String, Integer> getAccessoryStats()
     {
+        // Check if both arrays are equal length
         if (statNames.length != statValues.length)
         {
             throw new ArrayIndexOutOfBoundsException("statNames.length and statValues.length are not equal.");
