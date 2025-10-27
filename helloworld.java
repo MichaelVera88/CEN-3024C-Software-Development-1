99bd1e09a0f1cfab6bb61ee9b9fe4bfa3f36ec50
Michael Vera <michaelgvera88@gmail.com>
2025-10-27 14:05:29 -0400

    Cleaned Up Repo

--- helloworld.java
+++
@@ -1,9 +0,0 @@
-//Apologies, I had cleaned out my assignments from my previous Java courses.
-
-public class Main
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World");
-	}
-}
